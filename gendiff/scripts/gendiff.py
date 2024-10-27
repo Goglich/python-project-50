fd6da3728e7cde84912affb650908b950b2cb785
Goglich <ivant4221@gmail.com>
2024-10-27 22:09:54 +0300

    fix errors

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,5 @@
 import argparse
-from gendiff import generate_diff
+from gendiff.generate_diff import generate_diff
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(generate_diff.generate_diff(args.first_file, args.second_file, args.format))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == "__main__":
--- tests/test_gendifference.py
+++ tests/test_gendifference.py
@@ -1,4 +1,4 @@
-from gendiff import generate_diff
+from gendiff.generate_diff import generate_diff
 from tests.fixtures.fixture_paths import FILE1_JSON, FILE2_JSON, FILE1_YAML, FILE2_YAML, FILE1_YML, FILE2_YML, FILE3_JSON, FILE4_JSON
 
 
@@ -10,34 +10,34 @@ def excepted_result(path):
 
 def test_get_diff_json():
     excepted = excepted_result('tests/fixtures/excepted_result.txt')
-    diff = generate_diff.generate_diff(FILE1_JSON, FILE2_JSON)
+    diff = generate_diff(FILE1_JSON, FILE2_JSON)
     assert diff == excepted
 
 
 def test_get_diff_yaml():
     excepted = excepted_result('tests/fixtures/excepted_result.txt')
-    diff = generate_diff.generate_diff(FILE1_YAML, FILE2_YAML)
+    diff = generate_diff(FILE1_YAML, FILE2_YAML)
     assert diff == excepted
 
 
 def test_get_diff_yml():
     excepted = excepted_result('tests/fixtures/excepted_result.txt')
-    diff = generate_diff.generate_diff(FILE1_YML, FILE2_YML)
+    diff = generate_diff(FILE1_YML, FILE2_YML)
     assert diff == excepted
 
 
 def test_get_diff_nested_json():
     excepted = excepted_result('tests/fixtures/excepted_result_nested.txt')
-    diff = generate_diff.generate_diff(FILE3_JSON, FILE4_JSON)
+    diff = generate_diff(FILE3_JSON, FILE4_JSON)
     assert diff == excepted
 
 
 def test_get_diff_plain():
     excepted = excepted_result('tests/fixtures/excepted_result_plain.txt')
-    diff = generate_diff.generate_diff(FILE3_JSON, FILE4_JSON, 'plain')
+    diff = generate_diff(FILE3_JSON, FILE4_JSON, 'plain')
     assert diff == excepted
 
 def test_get_diff_format_json():
     excepted = excepted_result('tests/fixtures/excepted_result_json.txt')
-    diff = generate_diff.generate_diff(FILE3_JSON, FILE4_JSON, 'json')
+    diff = generate_diff(FILE3_JSON, FILE4_JSON, 'json')
     assert diff == excepted
